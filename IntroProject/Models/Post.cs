eae1b5c507c00900ff185f08ac0e51c5d0f98998
africko <africko@sandiego.edu>
2024-11-13 10:41:52 -0800

    Updated Data Models

--- IntroProject/Models/Post.cs
+++ IntroProject/Models/Post.cs
@@ -1,10 +1,16 @@
-﻿namespace IntroProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IntroProject.Models
 {
     public class Post
     {
-        public int Post_id { get; set; }
+        [Key] public int Post_id { get; set; }
         public string Contents { get; set; }
         public string User_id { get; set; }
+
+        [ForeignKey("User_id")]
+        public User User { get; set; }
 
     }
 }
--- IntroProject/Models/User.cs
+++ IntroProject/Models/User.cs
@@ -1,8 +1,10 @@
-﻿namespace IntroProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IntroProject.Models
 {
     public class User
     {
-        public int User_id { get; set; }
+        [Key] public int User_id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }

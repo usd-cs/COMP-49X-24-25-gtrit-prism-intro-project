ce9802954776a874e1db8738dda76c3166cefaaa
Aaron Fricko <113152618+aaron-fricko@users.noreply.github.com>
2024-11-16 14:28:56 -0800

    Updated WebForumDb Context to allow CRUD operations on web forum DB

--- IntroProject/Data/WebForumDbContext.cs
+++ IntroProject/Data/WebForumDbContext.cs
@@ -1,9 +1,17 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using IntroProject.Models;
 
 namespace IntroProject.Data
 {
-    public class WebForumDbContext(DbContextOptions<WebForumDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    public class WebForumDbContext : DbContext
     {
+        public WebForumDbContext(DbContextOptions<WebForumDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<IntroProject.Models.Comment> Comment { get; set; } = default!;
+        public DbSet<IntroProject.Models.Post> Post { get; set; } = default!;
+        public DbSet<IntroProject.Models.User> User { get; set; } = default!;
     }
 }

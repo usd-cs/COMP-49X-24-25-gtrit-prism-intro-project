dee37ca21503e94523ef627058c7be66ea532101
Aaron Fricko <113152618+aaron-fricko@users.noreply.github.com>
2024-11-19 19:56:59 -0800

    Added Admin Requirement Policy

---
+++ IntroProject/Policies/AdminRequirement.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace IntroProject.Policies;
+
+    public class AdminRequirement : IAuthorizationRequirement
+{
+    public class AdminAuthorizationHandler : AuthorizationHandler<AdminRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, AdminRequirement requirement)
+        {
+            if (context.User.HasClaim(c => c.Type == "Admin" && c.Value == "true"))
+            {
+                context.Succeed(requirement);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}

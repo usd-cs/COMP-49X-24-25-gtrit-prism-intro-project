99f41b4b851debce1b83766b3ab77e173ed00bf9
Aaron Fricko <113152618+aaron-fricko@users.noreply.github.com>
2024-11-19 20:44:12 -0800

    Added admin property to user

--- IntroProject/Data/ApplicationUser.cs
+++ IntroProject/Data/ApplicationUser.cs
@@ -5,6 +5,7 @@ namespace IntroProject.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public Boolean Admin { get; set; }
     }
 
 }
